cee11e165476be9dd6d439ad88a3fa04075d187b
Sudharsan Ananth <sudharsan.ananth@gmail.com>
2025-07-16 15:31:33 +0530

    added configurations for vite to host and publish the website

--- lummi-website/astro.config.mjs
+++ lummi-website/astro.config.mjs
@@ -5,5 +5,19 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  vite: {
+    server: {
+      host: '0.0.0.0'
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: process.env.PORT || 8080,
+      allowedHosts: ['all']
+    }
+  }
 });
