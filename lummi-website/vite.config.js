194d5fac10ffa20a3836a25d43e163405facc479
Sudharsan Ananth <sudharsan.ananth@gmail.com>
2025-07-16 15:35:48 +0530

    config change for all hosts

---
+++ lummi-website/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 8080,
+    allowedHosts: ['all']
+  }
+});
